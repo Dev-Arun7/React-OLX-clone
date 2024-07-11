2bad480b9e64802933971e0c47dc0151633ed333
Arun Balakrishnan <123arunbalakrishnan@gmail.com>
2024-07-11 10:34:25 +0530

    v1.20 manual routing is done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import Header from "./Components/Header/Header"
-import Banner from "./Components/Banner/Banner";
-import View from "./Components/View/View"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Ensure BrowserRouter is imported correctly
 
+import Home from './Pages/Home';
+import Create from './Pages/Create';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import ViewPost from './Pages/ViewPost';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Banner />
-      <View />
+      <BrowserRouter> {/* Wrap your Routes with BrowserRouter */}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} /> {/* Add '/' before login */}
+          <Route path='/create' element={<Create />} /> {/* Add '/' before create */}
+          <Route path='/view' element={<ViewPost />} /> {/* Add '/' before view */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App; 
+export default App;
